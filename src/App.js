d7dc6ebf4280451a2e1d4727c366b32e32626baa
SanchitBanati33 <86164665+SanchitBanati33@users.noreply.github.com>
2022-02-07 00:10:47 +0530

    Ticket FE V1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
+import Organizers from "./components/Pages/Organizers";
+import Tickets from "./components/Pages/Tickets";
+
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Tickets />
+      {/* <Organizers /> */}
+
+
+
+
+
 
     </div>
   );
